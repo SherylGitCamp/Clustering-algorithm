5aa7c0245b55e23a85ef95ec95d19d59e92cba3a
VVshoushou <w406971526@hotmail.com>
2018-05-18 15:21:26 +0200

    Add files via upload

---
+++ RF未完.js
@@ -0,0 +1,42 @@
+function exit() {
+  process.exit();
+}
+
+function stop(e) {
+  if (e) {
+    console.log(e);
+  }
+  sparkSession.stop().then(exit).catch(exit);
+}
+
+function run(sparkSession, spark){
+	return new Promise(function(resolve, reject) {
+
+    var root = process.env.EXAMPLE_ROOT || __dirname + "/.."
+  
+    var savedModel = new spark.ml.classification.RandomForestClassificationModel()
+        .load(root+"/model/saved_RF_gmm_0430_d4");
+
+    var test = 
+
+    var prediction = savedModel.transform(test).then(resolve).catch(reject);
+
+  });
+}
+
+if (global.SC) {
+  // we are being run as part of a test
+  module.exports = run;
+} else {
+  var eclairjs = require('eclairjs');
+  var spark = new eclairjs();
+  var sparkSession = spark.sql.SparkSession
+            .builder()
+            .appName("Random Forest Classifier")
+            .getOrCreate();
+
+  run(sparkSession, spark).then(function(results) {
+    console.log(results);
+    stop();
+  }).catch(stop);
+}
